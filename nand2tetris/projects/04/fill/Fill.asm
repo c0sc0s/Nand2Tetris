83edb72d2e594eaf2b3ad0d3e6ddee5d04610ec9
c0sc0s <yuwenjieshuai@gmail.com>
2023-01-17 12:33:29 +0800

    chapter 4

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -1,14 +1,40 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+@i
+M = -1
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(WAIT_KEYON)
+@KBD
+D = M
+@BLACK
+D;JNE
+@WHITE
+D;JEQ
+@WAIT_KEYON
+0;JMP
 
-// Put your code here.
+
+(BLACK)
+@SCREEN
+D = A
+@i
+M = M + 1
+A = D + M
+M = -1
+@WAIT_KEYON
+0;JMP
+
+
+(WHITE)
+@i
+D = M
+@WAIT_KEYON
+D;JLT
+
+@SCREEN
+D = A
+@i 
+A = D + M
+M = 0
+@i
+M = M - 1
+@WAIT_KEYON
+0;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,46 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// let sum = 0;
+// let i = 0;
+// while(i < R1){
+//   sum = sum + R0
+// }
+
+
+// let sum = 0
+@R2
+M = 0
+
+// let i = 0
+@i
+M = 0
+
+
+(LOOP)
+// while(i < R1) 
+// begin
+@i
+D = M
+@R1
+D = M - D//R1 - i > 0
+@END
+D;JLE
+// end
+
+
+// sum = sum + R0
+@R0
+D = M
+@R2
+M = D + M
+
+// i++
+@i
+M = M + 1
+
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
